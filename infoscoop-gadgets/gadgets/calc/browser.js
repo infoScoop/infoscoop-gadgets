f225b7270af8870e3116bb3fdd3697594cc7d02e
corestrike <corestrike@gmail.com>
2014-02-12 17:14:15 +0900

    fixes #15 電卓ガジェットの領域が引き延ばされる問題を修正

--- infoscoop-gadgets/gadgets/calc/browser.js
+++ infoscoop-gadgets/gadgets/calc/browser.js
@@ -9,22 +9,6 @@ Browser.isOpera = (navigator.userAgent.toLowerCase().indexOf("opera")!=-1);
 Browser.isSafari = (navigator.userAgent.toLowerCase().indexOf("safari")!=-1);
 Browser.Safari = {};
 Browser.Safari.version = (function() {
-	var versions = navigator.userAgent.match(/.*AppleWebKit\/(\d+).+Safari\/(\d+).*/);
-	if( !versions || versions.length < 2 )
-		return NaN;
-	
-	var webkitVer = parseInt( versions[1] );
-	var safariVer = parseInt( versions[2] );
-	
-	if( isNaN( webkitVer )|| isNaN( safariVer ))
-		return NaN;
-	
-	if( webkitVer < 412 ) {
-		return 1;
-	} else if( webkitVer < 522 ) {
-		return 2;
-	}
-	
 	return 3;
 })();
 Browser.isSafari1 = ( Browser.isSafari && Browser.Safari.version == 1 );
